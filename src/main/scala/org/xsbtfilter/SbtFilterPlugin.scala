69031d234aabe1d8496db46ba58cf2f8a7dad152
Russ Rollins <russell.c.rollins@gmail.com>
2011-06-10 14:40:03 -0600

    fixed compilation

--- src/main/scala/org/xsbtfilter/SbtFilterPlugin.scala
+++ src/main/scala/org/xsbtfilter/SbtFilterPlugin.scala
@@ -13,6 +13,7 @@ import Project.Initialize
 import java.util.{Enumeration, Properties => JProperties}
 import scala.collection.immutable._
 import java.io._
+import xsbti.api.Path
 
 object SbtFilterPlugin extends Plugin {
   val FilterResources = config("filter-resources")
@@ -24,7 +25,7 @@ object SbtFilterPlugin extends Plugin {
   //val filterExcludeFiles = SettingKey[PathFinder => PathFinder]("filter-exclude-files")
   val filterIncludeExtensions = SettingKey[Seq[String] => Seq[String]]("filter-include-extensions")
 
-  private def loadProperties(propFile:Reader): JProperties = {
+  private def loadProperties(propFile: Reader): JProperties = {
     val loeadedProps = new JProperties
     loeadedProps.load(propFile)
     propFile.close()
@@ -32,80 +33,88 @@ object SbtFilterPlugin extends Plugin {
   }
 
   private def filterResourcesTask: Initialize[Task[Unit]] =
-  (currentFilterEnvSetting,filterIncludeExtensions, baseDirectory,streams) map {
-      (curFilterEnvSetting, filterIncExts, baseDirectory, streamss) =>
+    (currentFilterEnvSetting, filterIncludeExtensions, baseDirectory, streams, target) map {
+      (curFilterEnvSetting, filterIncExts, baseDirectory, streamss, target) =>
 
-    def filterPath = baseDirectory / "src"/ "main" / "resources" / "filters"
-    def filterSources = filterPath * "*.properties"
-    streamss.log.info(filterPath.toString)
-
-
-
-    val envPropertyFilesMap = HashMap[String, String]() ++ filterSources.getPaths.map(path => {
-      val pathFile: java.io.File = new java.io.File(path)
-      pathFile.getName.split("\\.")(0) -> pathFile.absolutePath
-    })
-    streamss.log.info(envPropertyFilesMap.toString)
-    val envProps = loadProperties(new BufferedReader(new FileReader(envPropertyFilesMap(curFilterEnvSetting))))
-    streamss.log.info("Using the following properties for filtering " + envProps.toString)
-
-    //TODO add sbt-related property values
-
-//    if (filterPath.exists) {
-//      (mainResourcesOutputPath * "*").getFiles.foreach(x => filterResource(filterSources.get, x))
-//    } else {
-//      log.warn(filterPath.toString + " is missing")
-//    }
+        def filterPath = baseDirectory / "src" / "main" / "resources" / "filters"
+        def filterSources = filterPath * "*.properties"
+        streamss.log.info(filterPath.toString)
 
 
-//    streamss.log.info(SettingKey[String]("current-filter-env").toString)
-//    streamss.log.info(SettingKey[java.io.File]("base-directory").toString)
 
-    //streamss.log.info(filterPath.getClass.toString)
+        val envPropertyFilesMap = HashMap[String, String]() ++ filterSources.getPaths.map(path => {
+          val pathFile: java.io.File = new java.io.File(path)
+          pathFile.getName.split("\\.")(0) -> pathFile.absolutePath
+        })
+        streamss.log.info(envPropertyFilesMap.toString)
+        val envProps = loadProperties(new BufferedReader(new FileReader(envPropertyFilesMap(curFilterEnvSetting))))
+        streamss.log.info("Using the following properties for filtering " + envProps.toString)
 
-    //    def filterResource(filterFilePaths: Iterable[Path], dest: File): Unit = {
+        //TODO add sbt-related property values
+        def sbtBaseProps: Map[String, String] = {
+          Map(
+            "project.name" -> "TODO", // projectName.value,
+            "project.name" -> "TODO", //projectName.value,
+            "project.organization" -> "TODO", //projectOrganization.value,
+            "project.version" -> "TODO", //projectVersion.value.toString,
+            "build.scala.versions" -> "TODO", //buildScalaVersions.value,
+            "filter.env" -> currentFilterEnv)
+        }
 
-
-    //
-    //      def substitute(prop: JProperties, f: File) = {
-    //        val replacements: scala.collection.mutable.Map[String, String] = scala.collection.mutable.Map() ++ sbtBaseProps
-    //
-    //        //props file precedence is greater than sbt defaults
-    //        prop.propertyNames.foreach(key => {
-    //          replacements += (key.toString -> prop.getProperty(key.toString()))
-    //        })
-    //
-    //        if (f.isFile) {
-    //          val buf = new StringWriter
-    //          val in = Source.fromFile(f)
-    //          in.getLines.foreach(l => {
-    //            var line = l
-    //            //is there a more efficient way to do this?
-    //            replacements foreach {
-    //              case (key, value) =>
-    //                line = line.replaceAll("\\$\\{\\s*" + key.toString + "\\s*\\}", value)
-    //            }
-    //            buf.write(line)
-    //          })
-    //
-    //          val out = new PrintWriter(f)
-    //          out.print(buf.toString)
-    //          out.close()
-    //
-    //          //for reference purposes it might be helpful to store a master list of
-    //          //the properties that were used for filtering
-    //          //not sure where that would be ideally dumped
-    //        }
-    //      }
-    //
-    //      substitute(envProps, dest)
-    //}
+        //actual processing happens here, should put it in more appropriate location for readability
+        if (filterPath.exists) {
+          //TODO need the proper output path
+          (target * "*").get.foreach(filterResource)
+        } else {
+          streamss.log.error(filterPath.toString + " is missing!!")
+        }
 
 
-    //TODO add another property value the specify the list of extensions to include in filter replacement( .properties, .xml, .conf)
+        def filterResource(targetFile: java.io.File): Unit = {
+          implicit def javaEnumeration2Iterator[A](e: Enumeration[A]) = new Iterator[A] {
+            def next = e.nextElement
+            def hasNext = e.hasMoreElements
+          }
+
+          def substitute(prop: JProperties, f: java.io.File) = {
+            val replacements: scala.collection.mutable.Map[String, String] = scala.collection.mutable.Map() ++ sbtBaseProps
+
+            //props file precedence is greater than sbt defaults
+            prop.propertyNames.foreach(key => {
+              replacements += (key.toString -> prop.getProperty(key.toString()))
+            })
+
+            if (f.isFile) {
+              val buf = new StringWriter
+              val in = Source.fromFile(f)
+              in.getLines.foreach(l => {
+                var line = l
+                //is there a more efficient way to do this?
+                replacements foreach {
+                  case (key, value) =>
+                    line = line.replaceAll("\\$\\{\\s*" + key.toString + "\\s*\\}", value)
+                }
+                buf.write(line)
+              })
+
+              val out = new PrintWriter(f)
+              out.print(buf.toString)
+              out.close()
+
+              //for reference purposes it might be helpful to store a master list of
+              //the properties that were used for filtering
+              //not sure where that would be ideally dumped
+            }
+          }
+
+          substitute(envProps, targetFile)
+        }
 
 
-  }
+      //TODO add another property value the specify the list of extensions to include in filter replacement( .properties, .xml, .conf)
+
+
+    }
 
   //describedAs "Filter the main resource files for variable substitutions."
   /**
